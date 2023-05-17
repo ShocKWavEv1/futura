965caf2c0a44a18f1d0d4bd1e863a0557054bc11
rodd.sal <rodrigosalmeron@tuhabi.mx>
2023-05-17 02:16:14 -0600

    fix originals logo for xs screen

--- src/components/originals/components/originalsHeader/originalsHeader.tsx
+++ src/components/originals/components/originalsHeader/originalsHeader.tsx
@@ -5,7 +5,7 @@ import originals from "../../../../../public/assets/logo/originals.png";
 const OriginalsHeader: React.FC = () => {
   return (
     <Box w="100%" p="9rem 0rem 2rem 0rem">
-      <Box w={["400px", "400px", "500px", "500px"]}>
+      <Box w={["300px", "400px", "500px", "500px"]}>
         <Image priority src={originals} alt="Fvtvra Originals" />
       </Box>
     </Box>
