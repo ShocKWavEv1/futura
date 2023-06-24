8172d1e27a754c1cab56598707bee361d1a9b8e8
rodd.sal <rodrigosalmeron@tuhabi.mx>
2023-06-23 19:13:10 -0600

    no warning

--- src/components/pillsStepper/pillsStepper.tsx
+++ src/components/pillsStepper/pillsStepper.tsx
@@ -48,6 +48,7 @@ const PillStepper: React.FC<PillsStepperProps> = ({
         handleShoppingDrawer
       );
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [quantity]);
 
   const handleLoading = (val: boolean) => {
